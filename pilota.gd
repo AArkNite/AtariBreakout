909521a6a0c90a7f27dc4458078e11c556bee402
AArkNite <127753203+AArkNite@users.noreply.github.com>
2023-03-13 14:04:01 +0100

    Borrar textures
    He borrat la textura d'un tanc perquè no la necessito

--- pilota.gd
+++ pilota.gd
@@ -8,4 +8,9 @@ var moviment := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	gravity_scale = 1000
+	
 	pass # Replace with function body.
+func _process(delta):
+	gravity_scale = 0
+	
