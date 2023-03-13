ed9fd3bc99c16f257c04c8a24ca14ed7db3c8588
AArkNite <127753203+AArkNite@users.noreply.github.com>
2023-03-13 13:51:37 +0100

    Initial commit

---
+++ pilota.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+var moviment := Vector2.ZERO
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
