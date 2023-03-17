f5b3dbbfeb11031674545a6f6b4e88ba3647bf11
AArkNite <127753203+AArkNite@users.noreply.github.com>
2023-03-17 16:19:17 +0100

    Creat script main
    He creat un script anomenat main on ocorre el joc

---
+++ main.gd
@@ -0,0 +1,28 @@
+extends Node2D
+var totals := 89
+var jugant := true
+
+
+func _ready():
+	$contador.position = Vector2(420,590)
+	
+func _process(delta):
+	$puntuacio.suma(totals, len(get_tree().get_nodes_in_group("bloc")), jugant)
+	totals = len(get_tree().get_nodes_in_group("bloc"))
+	
+	if totals == 0:
+		$resultat.text = "WELL DONE!"
+		for j in get_tree().get_nodes_in_group("pilota"):
+			j.queue_free()
+		
+	if $contador.vides == 0:
+		jugant = false
+		for i in get_tree().get_nodes_in_group("bloc"):
+			i.queue_free()
+		for j in get_tree().get_nodes_in_group("pilota"):
+			j.queue_free()
+		$resultat.text = "GAME OVER"
+		
+		
+
+	
